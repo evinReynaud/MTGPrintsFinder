58c2e80a1f73dbe27f4fbfe312663acf15a0572a
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2024-10-19 13:46:24 +0200

    Update placeholder text

--- constants.js
+++ constants.js
@@ -1,1 +1,2 @@
-const syntaxText = `leb/233 (finds card #233 in Limited Edition Beta)`;
+const syntaxText = `Ancestral Recall (Finds all cards named "Ancestral Recall")
+leb/233 (finds all prints of card #233 in Limited Edition Beta (ie. Black Lotus))`;
